61b841e366a5a6d09f9195663ecee3848afa85d4
Brian Schmid <wischmi2@gmail.com>
2026-01-06 19:32:42 -0500

    Ignore local build outputs

--- build_gateway/gateway/git_describe.h
+++
@@ -1,1 +0,0 @@
-#define GIT_DESCRIBE "v0.2.0-54-g9008762-dirty"
--- build_gateway/gateway/tfm/api_ns/platform/common/core/config_nordic_nrf_spe.cmake
+++
@@ -1,7 +0,0 @@
-#
-# Copyright (c) 2023, Nordic Semiconductor ASA.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-#
-
-set(HAL_NORDIC_PATH C:/Users/Brian/pouch_gateway_clean/modules/hal/nordic CACHE STRING "Path to nordic HAL" FORCE)
